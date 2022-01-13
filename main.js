1f66ce3ae519e8213a4fd7b7175193a473256551
Luigi Pio Amendolagine <49374667+Lu191@users.noreply.github.com>
2022-01-13 18:55:39 +0100

    Fix

--- main.js
+++ main.js
@@ -1,25 +1,28 @@
 import { ApiPromise, WsProvider } from "@polkadot/api";
 
 async function main() {
-    // Test on Moonbase alpha, Pontem is down
-    // pontem ws provider: wss://testnet.pontem.network
-    const wsProvider = new WsProvider('wss://wss.api.moonbase.moonbeam.network');
+    // set ws provider
+    const wsProvider = new WsProvider('wss://testnet.pontem.network/ws');
     const api = await ApiPromise.create({ provider: wsProvider });
 
     // get collators selected in the active set
     let selectedCollators = await api.query.parachainStaking.selectedCandidates()
     selectedCollators = selectedCollators.map((authorityId) => authorityId.toString());                                 
-    console.log(selectedCollators);
+    console.log(`There are ${selectedCollators.length} active collators`);
 
-    /*let minBacking = 0;
-    selectedCollators.forEach(collator => {
-        let collatorState = await api.query.parachainStaking.collatorState2(collator);
-        if (minBacking == 0 || minBacking < collatorState.totalBacking) {
+    // get collator state's and display the one with less PONT
+    let collatorState = await api.query.parachainStaking.collatorState2(selectedCollators[0]);
+    let minBacking = collatorState.toJSON().totalBacking;
+    let minBackingCollator = selectedCollators[0];
+    for (let index = 1; index < selectedCollators.length; index++) {
+        collatorState = await api.query.parachainStaking.collatorState2(selectedCollators[index]);
+        collatorState = collatorState.toJSON();
+        if (minBacking > collatorState.totalBacking) {
             minBacking = collatorState.totalBacking;
-            minBackingCollator = collator;
+            minBackingCollator = selectedCollators[index];
         }
-    });
-    console.log(`Min backing: ${minBackingCollator} with ${minBacking} PONT`);*/
+    }
+    console.log(`Min backing: ${minBackingCollator} with ${minBacking / 10000000000} PONT`);
 }
 
 main().catch(console.error).finally(() => process.exit());
